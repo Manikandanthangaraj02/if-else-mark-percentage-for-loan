8e118fa35db8c777a78c104328cee35362b2870b
Manikandanthangaraj02 <110436810+Manikandanthangaraj02@users.noreply.github.com>
2023-09-04 11:26:47 +0530

    Create eligibility-check-for-loan.py

---
+++ eligibility-check-for-loan.py
@@ -0,0 +1,8 @@
+a=int(input("give score percentage:"))
+if(a>=70):
+    name=input("name:")
+    department=input("department:")
+    location=input("location:")
+    print("eligible")
+else:
+    print("not eligible")
